0de0db9729dce6a6ced958840e48c86d0c0a544d
Aditya Choubey <97503774+Aadi0706@users.noreply.github.com>
2022-06-09 20:45:17 +0530

    debouncing updated

--- Debouncing/index.js
+++ Debouncing/index.js
@@ -1,1 +1,20 @@
+var button = document.getElementById("button");
 
+function deboounce(func, delay) {
+  let deboounce;
+
+  return function () {
+    if (deboounce) {
+      clearTimeout(deboounce);
+    }
+
+    deboounce = setTimeout(() => {
+      func();
+    }, delay);
+  };
+}
+
+button.addEventListener("click", deboounce(function(){
+    alert("debouncing in 4 seconds")
+},4000)
+);
